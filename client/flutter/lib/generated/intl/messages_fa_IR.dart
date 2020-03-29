739dc46e22743a600af42d711cae96884ef7edae
Mantab Tadayon <mt.tadayon@gmail.com>
2020-03-29 16:23:12 +0200

    feat: Added farsi translation ARB file

---
+++ client/flutter/lib/generated/intl/messages_fa_IR.dart
@@ -0,0 +1,73 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a fa_IR locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'fa_IR';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "commonWorldHealthOrganization" : MessageLookupByLibrary.simpleMessage("سازمان بهداشت جهانی"),
+    "commonWorldHealthOrganizationCoronavirusApp" : MessageLookupByLibrary.simpleMessage("WHO Coronavirus App"),
+    "healthCheckTitle" : MessageLookupByLibrary.simpleMessage("Health Check"),
+    "homePagePageButtonLatestNumbers" : MessageLookupByLibrary.simpleMessage("آخرین آمار"),
+    "homePagePageButtonProtectYourself" : MessageLookupByLibrary.simpleMessage("محافظت از خود"),
+    "homePagePageButtonTravelAdvice" : MessageLookupByLibrary.simpleMessage("توصیه های سفر"),
+    "homePagePageButtonWHOMythBusters" : MessageLookupByLibrary.simpleMessage("WHO Myth-busters"),
+    "homePagePageButtonYourQuestionsAnswered" : MessageLookupByLibrary.simpleMessage("جواب سوالات شما"),
+    "homePagePageSliverListAboutTheApp" : MessageLookupByLibrary.simpleMessage("درباره اپلیکیشن"),
+    "homePagePageSliverListAboutTheAppDialog" : MessageLookupByLibrary.simpleMessage("اپلیکیشن رسمی کووید ۱۹ سازمان بهداشت جهانی"),
+    "homePagePageSliverListProvideFeedback" : MessageLookupByLibrary.simpleMessage("ارائه بازخورد"),
+    "homePagePageSliverListShareTheApp" : MessageLookupByLibrary.simpleMessage("اشتراک گذاری اپلیکیشن"),
+    "protectYourselfListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("دستان خود را حداقل ۲۰ ثانیه با صابون و آب بشویید"),
+    "protectYourselfListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("از دست زدن به چشم، دهان و بینی خودداری کنید"),
+    "protectYourselfListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("هنگام سرفه یا عطسه، دهان و بینی خود را با خم آرنج خود یا دستمال کاغذی بپوشانید"),
+    "protectYourselfListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("بین شما و کسی که در حال عطسه یا سرفه است باید حداقل 1 متر  (> 3 فوت) فاصله وجود داشته باشد"),
+    "protectYourselfListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("استفاده از ماسک فقط براي افرادي كه علائم کووید ۱۹ دارند و براي افرادي كه از كسي با این علایم مراقبت مي كنند توصیه می شود. (به خصوص علائمي مانند سرفه)"),
+    "protectYourselfTitle" : MessageLookupByLibrary.simpleMessage("محافظت از خود"),
+    "travelAdviceListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("سازمان جهانی بهداشت برای کشورهایی که شیوع کووید ۱۹ را تجربه می کنند در مورد اعمال محدودیت های مسافرتی یا تجاری  توصیه می کند"),
+    "travelAdviceListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("Only wear a mask if you are ill with COVID-19 symptoms (especially coughing) or looking after someone who may have COVID-19"),
+    "travelAdviceListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("Travellers returning from affected areas should:"),
+    "travelAdviceListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("Self-monitor for symptoms for 14 days and follow national protocols of receiving countries. Some countries may require returning travellers to enter quarantine"),
+    "travelAdviceListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("Thermal scanners CAN detect if people have a fever but CANNOT detect whether or not someone has the coronavirus"),
+    "travelAdviceListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("If symptoms occur, such as fever, or cough or difficulty breathing, travellers are advised to contact local health care providers, preferably by phone, and inform them of their symptoms and their travel history"),
+    "travelAdviceListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("به مسافرانی که بیمار هستند، به ویژه برای مسافران سالخورده و افراد مبتلا به بیماری های مزمن، توصیه می شود که از مسافرت کردن به مناطق آسیب دیده پرهیز کنند"),
+    "travelAdviceListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("“Affected areas” are considered those countries, provinces, territories or cities experiencing ongoing transmission of COVID-19, in contrast to areas reporting only imported cases…"),
+    "travelAdviceListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("General recommendations for all travellers include…"),
+    "travelAdviceListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("Wash your hands frequently"),
+    "travelAdviceListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("Avoid touching your eyes, mouth and nose"),
+    "travelAdviceListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("Cover your mouth and nose with your bent elbow or tissue when you cough or sneeze"),
+    "travelAdviceListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("Stay more than 1 meter (3 feet) away from a person who is sick"),
+    "travelAdviceListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("Follow proper food hygiene practices"),
+    "whoMythBustersListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("There is a lot of false information around. These are the facts"),
+    "whoMythBustersListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("Thermal scanners CAN detect if people have a fever but CANNOT detect whether or not someone has the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("Spraying alcohol or chlorine all over your body WILL NOT kill viruses that have already entered your body"),
+    "whoMythBustersListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("Vaccines against pneumonia, such as pneumococcal vaccine and Haemophilus influenzae type b (Hib) vaccine, DO NOT provide protection against the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("There is NO evidence that regularly rinsing the nose with saline has protected people from infection with the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("Garlic is healthy but there is NO evidence from the current outbreak that eating garlic has protected people from the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem15" : MessageLookupByLibrary.simpleMessage("Antibiotics DO NOT work against viruses, antibiotics only work against bacteria"),
+    "whoMythBustersListOfItemsPageListItem16" : MessageLookupByLibrary.simpleMessage("To date, there is NO specific medicine recommended to prevent or treat the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("People of all ages CAN be infected by the coronavirus. Older people, and people with pre-existing medical conditions (such as asthma, diabetes, heart disease) appear to be more vulnerable to becoming severely ill with the virus"),
+    "whoMythBustersListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("Cold weather and snow CANNOT kill the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("The coronavirus CAN be transmitted in areas with hot and humid climates"),
+    "whoMythBustersListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("The coronavirus CANNOT be transmitted through mosquito bites"),
+    "whoMythBustersListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("There is NO evidence that companion animals/pets such as dogs or cats can transmit the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("Taking a hot bath DOES NOT prevent the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("Hand dryers are NOT effective in killing the coronavirus"),
+    "whoMythBustersListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("Ultraviolet light SHOULD NOT be used for sterilization and can cause skin irritation")
+  };
+}
